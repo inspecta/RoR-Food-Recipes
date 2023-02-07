8a1b98a303dbe6cea0059f97f309ddcdb9e29fe4
Derrick Mulinde <djmulinde@gmail.com>
2023-02-07 15:38:40 +0300

    Create new rails app

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,13 +10,6 @@ module RoRFoodRecipes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
 
     # Configuration for the application, engines, and railties goes here.
     #
--- db/migrate/20230207100943_change_users_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUsersColumnName < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :Name, :name
-  end
-end
