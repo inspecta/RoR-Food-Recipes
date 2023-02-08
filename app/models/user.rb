e110af120bcaf8d218e2e784986e27ebbd780ac0
Joel-100 <joelkugonza@gmail.com>
2023-02-08 11:39:18 +0300

    Update users file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   validates :name, presence: true, length: { minimum: 2 }
 end
