8ce3966f97cf832faa3c39c0a87ab5062fbe01c7
Derrick Mulinde <djmulinde@gmail.com>
2023-02-10 12:47:38 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :recipes, only: [:index, :show, :new, :create, :destroy]
   end
+
+  resources :public_recipes, only: [:index]
 end
